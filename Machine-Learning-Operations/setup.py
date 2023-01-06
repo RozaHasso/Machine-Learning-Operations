5c06f706b845116f1ee46642eaa3176797729a40
Maqerfoo <jakobpoulsen93@gmail.com>
2023-01-06 10:52:33 +0100

    cookiecutter

---
+++ Machine-Learning-Operations/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Image classification on cats and dogs',
+    author='Group29',
+    license='MIT',
+)
